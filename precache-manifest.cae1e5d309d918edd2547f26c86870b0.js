bc6b896337bcf9c95624eefc849ad87f129e5007
vlad314 <savu_vlad@yahoo.com>
2020-09-01 01:02:12 +0300

    Updates

---
+++ precache-manifest.cae1e5d309d918edd2547f26c86870b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "301dd592f723e599b7c330524727d381",
+    "url": "/OnScreenKeyboard/index.html"
+  },
+  {
+    "revision": "71b339455d265b01fd94",
+    "url": "/OnScreenKeyboard/static/css/main.0c3b5451.chunk.css"
+  },
+  {
+    "revision": "54862776d75be52c2455",
+    "url": "/OnScreenKeyboard/static/js/2.a8cce41f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/OnScreenKeyboard/static/js/2.a8cce41f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71b339455d265b01fd94",
+    "url": "/OnScreenKeyboard/static/js/main.b4d9f922.chunk.js"
+  },
+  {
+    "revision": "c485b2ed43d97d9d0260",
+    "url": "/OnScreenKeyboard/static/js/runtime-main.4bcdfd2e.js"
+  }
+]);
